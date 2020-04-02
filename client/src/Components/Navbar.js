f5205cab49a9c61eec1640ae3d06e0bf2e5f928c
Ganesh Dasari <gdasari74@gmail.com>
2020-04-03 01:04:51 +0530

    update on signup page

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,24 +1,29 @@
 import React from "react";
 import Panda from "./Panda.png";
+import { Link } from "react-router-dom";
+import "./../style/navbar.css";
 
-class Navbar extends React.Component{
-    render(){
-        return(
-            <div>
-                <nav class="navbar navbar-light bg-light py-0">
-                        <div className="d-flex justify-content-start ">
-                            <button class="btn">Home</button>
-                            <button class="btn">About Us</button>
-                        </div>
-                        <img src={Panda} alt="" style={{"width":"50px"}}/>
-                        <div className="d-flex justify-content-end">
-                            <button class="btn">Contact Us</button>
-                            <button class="btn">SignIn</button>
-                        </div>
-                </nav>
-            </div>
-        )
-    }
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-light bg-light py-0">
+          <div className="d-flex justify-content-start ">
+            <button className="btn">Home</button>
+            <button className="btn">About Us</button>
+          </div>
+          <img src={Panda} alt="" style={{ width: "50px" }} />
+          <div className="d-flex justify-content-end">
+            <button className="btn">Contact Us</button>
+
+            <Link to="/signin" className="btn">
+              Signin
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
