0ba89844051744933d684e8134f7e9f3b04a224a
manoj-kumar-vst-au4 <mauryamanoj513@gmail.com>
2020-04-03 12:46:02 +0530

    footer created

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,28 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import "./style/app.css";
 
 import Signin from "./Components/Signin";
 class App extends React.Component {
   render() {
     return (
       <Fragment>
-        <div className="container-fluid-lg">
-          <Router>
-            <Navbar />
-            <Route exact path="/" component={Home} />
+        <div className="container-fluid-lg" id="container">
+          <div id="wrapper">
+            <Router>
+              <Navbar />
+              <Route exact path="/" component={Home} />
 
-            <Route exact path="/signin" component={Signin} />
-          </Router>
+              <Route exact path="/signin" component={Signin} />
+            </Router>
+          </div>
+          <div id="footer">
+            <Footer/>
+          </div>
         </div>
+        
       </Fragment>
     );
   }
