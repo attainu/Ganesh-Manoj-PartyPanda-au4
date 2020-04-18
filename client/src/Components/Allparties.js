c6c186b6665e0cb9620a3736e359f9cfe96259aa
Ganesh Dasari <gdasari74@gmail.com>
2020-04-18 19:11:15 +0530

    update

--- client/src/Components/Allparties.js
+++ client/src/Components/Allparties.js
@@ -1,19 +1,18 @@
 import React, { Fragment } from "react";
-import {Redirect} from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Dome from "./../images/dome.jpg";
 import "./../style/allparties.css";
 
-
 class Allparties extends React.Component {
- sendId =(data) =>{
-   let id = data._id;
-   console.log(id);
-  this.props.dispatch({type:"eventId", payload: id})
- }
+  sendId = (data) => {
+    let id = data._id;
+    console.log(id);
+    this.props.dispatch({ type: "eventId", payload: id });
+  };
   render() {
-    if(this.props.selectedEventId){
-      return <Redirect to="/event-detail" />
+    if (this.props.selectedEventId) {
+      return <Redirect to="/event-detail" />;
     }
     let allEvent = this.props.allEvent;
     return (
@@ -24,24 +23,37 @@ class Allparties extends React.Component {
             <hr />
           </center>
           <div className="d-flex flex-row flex-wrap justify-content-around">
-            {allEvent.map(item =>{
-              return(
-                <div className="card event mb-5" style={{ width: "35vw" }} onClick={() =>this.sendId(item)}>
-              <img className="card-img-top" src={item.image} alt="Card image cap"  style={{"height":"300px","width":"100%"}}/>
-              <div className="card-body ">
-                <div className="d-flex flex-row justify-content-between flex-wrap pb-2">
-                  <h5 className="card-title">{item.host.name}'s {item.theme}</h5>
-                  <p className="card-text align-self-center pt-3">
-                    {item.type}
-                  </p>
+            {allEvent.map((item) => {
+              return (
+                <div
+                  className="card event mb-5"
+                  style={{ width: "35vw" }}
+                  onClick={() => this.sendId(item)}
+                >
+                  <img
+                    className="card-img-top"
+                    src={item.image}
+                    alt="Card image cap"
+                    style={{ height: "300px", width: "100%" }}
+                  />
+                  <div className="card-body ">
+                    <div className="d-flex flex-row justify-content-between flex-wrap pb-2">
+                      <h5 className="card-title">
+                        {item.host.name}'s {item.theme}
+                      </h5>
+                      <p className="card-text align-self-center pt-3">
+                        {item.type}
+                      </p>
+                    </div>
+                    <div className="d-flex flex-row justify-content-between flex-wrap pb-2">
+                      <h5 className="card-text">{item.location}</h5>
+                      <p className="card-text">
+                        {item.date} | {item.start_time} to {item.end_timing}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div className="d-flex flex-row justify-content-between flex-wrap pb-2">
-                  <h5 className="card-text">{item.location}</h5>
-              <p className="card-text">{item.date} |  {item.start_time} to {item.end_timing}</p>
-                </div>
-              </div>
-            </div>
-              )
+              );
             })}
           </div>
         </div>
@@ -54,7 +66,7 @@ const fromStroe = (state) => {
   return {
     show: state.show,
     allEvent: state.allEvent,
-    selectedEventId: state.selectedEventId
+    selectedEventId: state.selectedEventId,
   };
 };
 export default connect(fromStroe)(Allparties);
