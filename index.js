ed36e8991fa08f52e854053b799f8b98f52e72ec
Ganesh Dasari <gdasari74@gmail.com>
2020-04-27 23:15:15 +0530

    update

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ const db = require("./Model/index.js");
 const port = process.env.PORT || 3010;
 
 let server = http.createServer(app);
-// Server Static assets if in production
-if (port === "production") {
-  // Set a static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+
 server
   .listen(port, function () {
     console.log("Server has started at port number 3010!");
