a64f732d0b4a269d5b10851ed14f119f4a2f2ccf
Ganesh Dasari <gdasari74@gmail.com>
2020-04-28 14:25:50 +0530

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   serviceID: "VAce4b58fa62749b8d8aca0264b18ca67e",
   accountSID: "ACf4322ed1358a61cf0db7eb92a9d21e28",
-  authToken: "f1bc51f3b25f8803a30af99704c10918",
+  authToken: "f9f1fc0246c12e558c4d70acd0350ad3",
 };
 
 // mongodb+srv://root:<password>@cluster0-vmkir.mongodb.net/test?retryWrites=true&w=majority
